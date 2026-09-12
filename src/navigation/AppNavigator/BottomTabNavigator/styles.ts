d137633916714c0337eaddf17c4a2888a0413f37
Faizul-Hasan <faizulhasan7861@gmail.com>
2026-09-12 16:42:01 +0530

    bottomTab icons implemented

--- src/navigation/AppNavigator/BottomTabNavigator/styles.ts
+++ src/navigation/AppNavigator/BottomTabNavigator/styles.ts
@@ -1,9 +1,19 @@
 import { StyleSheet } from 'react-native';
-import { fontSizes, fonts } from '@utilities/constants/common';
+import { Percentages, fontSizes, fonts } from '@utilities/constants/common';
+import { DimensionValue } from 'react-native';
 
 export const TabBarStyle = StyleSheet.create({
   tabBarlabel: {
-    fontSize: fontSizes.small,
-    fontFamily: fonts.PrimaryMedium,
+    fontSize: fontSizes.extraSmall,
+    fontFamily: fonts.Primary,
+  },
+  iconStyle: {
+    width: 25,
+    height: 25,
+  },
+  profileIconStyle: {
+    width: Percentages.hundred as DimensionValue,
+    height: Percentages.hundred as DimensionValue,
+    borderRadius: 50,
   },
 });
